e279d2c0ac2d0986f6dbbad617aaf0b1bf628934
Brian J. Burroughs <objectbb@gmail.com>
2015-08-01 20:22:30 -0500

    code cleanup

--- BB_Practicum_API/Rules/AbstractRule.cs
+++
@@ -1,9 +0,0 @@
-﻿using System;
-namespace BB_Practicum_API.Rules
-{
-    public abstract class AbstractRule: IRule
-    {
-        public string ErrorMsg { get { return "Error"; } }
-        public abstract string ReturnMsg(IDish dish, int? pos = null);
-    }
-}
--- BB_Practicum_API/Rules/AbstractRuleMultipleOrder.cs
+++
@@ -1,9 +0,0 @@
-﻿using System;
-namespace BB_Practicum_API.Rules
-{
-    public abstract class AbstractRuleMultipleOrder : IRule
-    {
-        public string ErrorMsg { get { return "Error"; } }
-        public abstract string ReturnMsg(IDish dish, int? pos = null);
-    }
-}
